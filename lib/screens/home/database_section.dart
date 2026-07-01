16592415fe36b471283550d5b4d4577e9b2e4cc7
edzen12 <oichiev.edzen@gmail.com>
2026-07-01 16:59:34 +0600

    edit database

--- lib/screens/home/database_section.dart
+++ lib/screens/home/database_section.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
 
 class DatabaseSection extends StatelessWidget {
   const DatabaseSection({super.key});
@@ -6,7 +7,47 @@ class DatabaseSection extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Expanded(
-      child: Container(width: double.infinity, color: Colors.white),
+      child: Container(
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.only(
+            topLeft: Radius.circular(10),
+            topRight: Radius.circular(10),
+          ),
+          color: Colors.white,
+        ),
+        padding: const EdgeInsets.only(top: 20, left: 20, right: 20),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text(
+              'Последние сохраненные зикры',
+              style: TextStyle(fontWeight: FontWeight.bold),
+            ),
+            Container(
+              width: 60,
+              height: 2,
+              color: Colors.blue,
+              margin: EdgeInsets.only(top: 3, bottom: 20),
+            ),
+            Expanded(
+              child: ListView.builder(
+                itemCount: 10,
+                itemBuilder: ((context, index) {
+                  return Container(
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(10),
+                      color: Colors.grey,
+                    ),
+                    width: double.infinity,
+                    margin: const EdgeInsets.only(bottom: 10),
+                    child: Row(),
+                  );
+                }),
+              ),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
