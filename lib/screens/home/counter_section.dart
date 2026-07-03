b9452cc0ed4a82501823226b9b10143bae941e14
edzen12 <oichiev.edzen@gmail.com>
2026-07-03 17:35:47 +0600

    counter buttons section

--- lib/screens/home/counter_section.dart
+++ lib/screens/home/counter_section.dart
@@ -1,8 +1,40 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/svg.dart';
 
-class CounterSection extends StatelessWidget {
+// Stateless - неизменяемые
+// Stateful- неизменяемые
+
+class CounterSection extends StatefulWidget {
   const CounterSection({super.key});
+
+  @override
+  State<CounterSection> createState() => _CounterSectionState();
+}
+
+class _CounterSectionState extends State<CounterSection> {
+  int counter = 0;
+
+  void increment() {
+    setState(() {
+      counter++;
+    });
+  }
+
+  void decrement() {
+    if (counter > 0) {
+      setState(() {
+        counter--;
+      });
+    }
+  }
+
+  void zeroing() {
+    if (counter != 0) {
+      setState(() {
+        counter = 0;
+      });
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -20,54 +52,63 @@ class CounterSection extends StatelessWidget {
             child: Row(
               mainAxisAlignment: MainAxisAlignment.spaceEvenly,
               children: [
-                Container(
-                  decoration: BoxDecoration(
-                    borderRadius: BorderRadius.circular(10),
-                    color: Colors.lightBlueAccent,
-                  ),
-                  width: 35,
-                  height: 35,
-                  alignment: Alignment.center,
-                  child: SvgPicture.asset('assets/icons/minus.svg'),
-                ),
-                Container(
-                  decoration: BoxDecoration(
-                    color: Colors.blue,
-                    borderRadius: BorderRadius.circular(10),
-                  ),
-                  width: 155,
-                  height: 155,
-                  child: Column(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      Text(
-                        '33',
-                        style: TextStyle(
-                          fontSize: 55,
-                          color: Colors.white,
-                          fontWeight: FontWeight.bold,
-                        ),
-                      ),
-                      Text(
-                        'Dhikr',
-                        style: TextStyle(
-                          fontSize: 16,
-                          color: Colors.white,
-                          fontWeight: FontWeight.bold,
-                        ),
-                      ),
-                    ],
+                GestureDetector(
+                  onTap: () => decrement(),
+                  child: Container(
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(10),
+                      color: Colors.lightBlueAccent,
+                    ),
+                    width: 35,
+                    height: 35,
+                    alignment: Alignment.center,
+                    child: SvgPicture.asset('assets/icons/minus.svg'),
                   ),
                 ),
-                Container(
-                  decoration: BoxDecoration(
-                    borderRadius: BorderRadius.circular(10),
-                    color: Colors.lightBlueAccent,
+                GestureDetector(
+                  onTap: () => increment(),
+                  child: Container(
+                    decoration: BoxDecoration(
+                      color: Colors.blue,
+                      borderRadius: BorderRadius.circular(10),
+                    ),
+                    width: 155,
+                    height: 155,
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Text(
+                          '$counter',
+                          style: TextStyle(
+                            fontSize: 55,
+                            color: Colors.white,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                        Text(
+                          'Dhikr',
+                          style: TextStyle(
+                            fontSize: 16,
+                            color: Colors.white,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                      ],
+                    ),
                   ),
-                  width: 35,
-                  height: 35,
-                  alignment: Alignment.center,
-                  child: SvgPicture.asset('assets/icons/reload.svg'),
+                ),
+                GestureDetector(
+                  onTap: () => zeroing(),
+                  child: Container(
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(10),
+                      color: Colors.lightBlueAccent,
+                    ),
+                    width: 35,
+                    height: 35,
+                    alignment: Alignment.center,
+                    child: SvgPicture.asset('assets/icons/reload.svg'),
+                  ),
                 ),
               ],
             ),
